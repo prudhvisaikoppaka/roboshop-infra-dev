a6e8f87bd7371fbdcf988af2161a07545b7ea9f6
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-13 18:13:03 +0530

    security group created

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -9,7 +9,6 @@ module "frontend" {
   vpc_id         = local.vpc_id
 }
 
-
 module "bastion" {
   #source      = "../../terraform-aws-securitygroup"
   source      = "git::https://github.com/prudhvisaikoppaka/terraform-aws-securitygroup.git?ref=main"
@@ -21,6 +20,28 @@ module "bastion" {
   vpc_id         = local.vpc_id
 }
 
+module "backend_alb" {
+  #source      = "../../terraform-aws-securitygroup"
+  source      = "git::https://github.com/prudhvisaikoppaka/terraform-aws-securitygroup.git?ref=main"
+  project     = var.project
+  environment = var.environment
+
+  sg_name        = var.backend_alb_sg_name
+  sg_description = var.backend_alb_sg_description
+  vpc_id         = local.vpc_id
+}
+
+module "vpn" {
+  #source      = "../../terraform-aws-securitygroup"
+  source      = "git::https://github.com/prudhvisaikoppaka/terraform-aws-securitygroup.git?ref=main"
+  project     = var.project
+  environment = var.environment
+
+  sg_name        = "vpn"
+  sg_description = "for vpn"
+  vpc_id         = local.vpc_id
+}
+
 # bastion accepting connections from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
@@ -29,4 +50,61 @@ resource "aws_security_group_rule" "bastion_laptop" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.bastion.sg_id
+}
+
+# backend ALB accepting connections from my bastion host on port no 80
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id        = module.backend_alb.sg_id
+}
+
+# vpn ports are 22, 443, 1194, 943
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+# backend ALB accepting connections from my VPN on port no 80
+resource "aws_security_group_rule" "backend_alb_vpn" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id        = module.backend_alb.sg_id
 }
--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -21,3 +21,11 @@ variable "bastion_sg_name" {
 variable "bastion_sg_description" {
   default = "created sg for bastion instance"
 }
+
+variable "backend_alb_sg_name" {
+  default = "backend-alb"
+}
+
+variable "backend_alb_sg_description" {
+  default = "created sg for backend alb instance"
+}
