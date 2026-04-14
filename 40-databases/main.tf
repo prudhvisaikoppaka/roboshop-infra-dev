76c33821eaef400828f539ea95b3f33c927cb04b
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-14 23:50:25 +0530

    databases created

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,16 +37,17 @@ resource "terraform_data" "mongodb" {
   }
 }
 
-resource "aws_instance" "mongodb" {
+
+resource "aws_instance" "redis" {
   ami                    = local.ami_id
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [local.mongodb_sg_id]
+  vpc_security_group_ids = [local.redis_sg_id]
   subnet_id              = local.database_subnet_id
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.environment}-mongodb"
+      Name = "${var.project}-${var.environment}-redis"
     }
   )
 }
@@ -75,6 +76,7 @@ resource "terraform_data" "redis" {
     ]
   }
 }
+
 
 resource "aws_instance" "mysql" {
   ami                    = local.ami_id
@@ -114,6 +116,7 @@ resource "terraform_data" "mysql" {
     ]
   }
 }
+
 
 resource "aws_instance" "rabbitmq" {
   ami                    = local.ami_id
