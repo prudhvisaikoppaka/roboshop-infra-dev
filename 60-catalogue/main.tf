3de98a70040725ea5fdc272b1bc4ff09d8c56267
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-17 23:09:33 +0530

    catalogue installed

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -52,4 +52,16 @@ resource "terraform_data" "catalogue" {
       "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
     ]
   }
-}  
+}
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on  = [terraform_data.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = "aws_instance.catalogue.id"
+  depends_on         = [aws_ec2_instance_state.catalogue]
+}
