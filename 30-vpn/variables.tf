4008aa1cd4db697b671830eed3348d46c474ea5b
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-17 13:49:42 +0530

    vpn setup

--- 30-vpn/variables.tf
+++ 30-vpn/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z08496491D9BJEMR7DC94"
+}
+
+variable "zone_name" {
+  default = "prudhvisai.space"
+} 
