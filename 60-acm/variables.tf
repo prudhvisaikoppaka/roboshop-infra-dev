d927f82f6015b7b4b693e18b40a40b486f7aa5a4
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-25 16:12:04 +0530

    aws acm certificate

---
+++ 60-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z08496491D9BJEMR7DC94"
+}
+
+variable "zone_name" {
+  default = "prudhvisai.space"
+}
