24f20a788e3855ada12f04850092bcc0638c8289
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-28 10:28:53 +0530

    user component installed

--- 80-user/main.tf
+++ 80-user/main.tf
@@ -1,6 +1,6 @@
 module "user" {
-    source = "../../terraform-aws-roboshop"
-    component = "user"
-    rule_priority = 20
+  source        = "../../terraform-aws-roboshop"
+  component     = "user"
+  rule_priority = 20
 
 }
