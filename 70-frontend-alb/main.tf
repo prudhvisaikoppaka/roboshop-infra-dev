a7c9532be7143bc4c2327d1f2cb917ea4584d64e
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-28 10:29:47 +0530

    frontend alb setup

--- 70-frontend-alb/main.tf
+++ 70-frontend-alb/main.tf
@@ -1,14 +1,14 @@
- module "frontend_alb" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "9.16.0"
-  internal = false
-  name                  = "${var.project}-${var.environment}-frontend-alb" # roboshop-dev-frontend-alb
-  vpc_id                = local.vpc_id
-  subnets               = local.public_subnet_ids
-  create_security_group = false
-  security_groups       = [local.frontend_alb_sg_id]
+module "frontend_alb" {
+  source                     = "terraform-aws-modules/alb/aws"
+  version                    = "9.16.0"
+  internal                   = false
+  name                       = "${var.project}-${var.environment}-frontend-alb" # roboshop-dev-frontend-alb
+  vpc_id                     = local.vpc_id
+  subnets                    = local.public_subnet_ids
+  create_security_group      = false
+  security_groups            = [local.frontend_alb_sg_id]
   enable_deletion_protection = false
-  
+
   tags = merge(
     local.common_tags,
     {
@@ -23,7 +23,7 @@ resource "aws_lb_listener" "frontend_alb" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn = local.acm_certificate_arn
+  certificate_arn   = local.acm_certificate_arn
   default_action {
     type = "fixed-response"
 
@@ -33,7 +33,7 @@ resource "aws_lb_listener" "frontend_alb" {
       status_code  = "200"
     }
   }
-}  
+}
 
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
