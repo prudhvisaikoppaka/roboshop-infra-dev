bd9f2f4739ffb1e81a746bc3b98741ca3d705a4c
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-28 11:14:33 +0530

    terraform aws roboshop infra setup

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,5 +1,11 @@
 variable "components" {
   default = {
+    catalogue = {
+      rule_priority = 10
+    }
+    user = {
+      rule_priority = 20
+    }
     cart = {
       rule_priority = 30
     }
