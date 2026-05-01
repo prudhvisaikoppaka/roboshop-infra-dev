5bf7684461e343d6e881282f3de516ed73af7d13
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-01 22:18:17 +0530

    terraform frontend alb installed

--- 70-frontend-alb/data.tf
+++ 70-frontend-alb/data.tf
@@ -10,6 +10,6 @@ data "aws_ssm_parameter" "frontend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
 
-# data "aws_ssm_parameter" "acm_certificate_arn" {
-#   name = "/${var.project}/${var.environment}/acm_certificate_arn"
-# }
+data "aws_ssm_parameter" "acm_certificate_arn" {
+  name = "/${var.project}/${var.environment}/acm_certificate_arn"
+}
