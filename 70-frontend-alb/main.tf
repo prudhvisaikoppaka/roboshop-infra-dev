e70e26872772fb4e10bc750bd7205f6d10254dfd
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-01 23:03:32 +0530

    terraform frontend alb installed

--- 70-frontend-alb/main.tf
+++ 70-frontend-alb/main.tf
@@ -23,7 +23,7 @@ resource "aws_lb_listener" "frontend_alb" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local.acm_certificate_arn
+  # certificate_arn   = local.acm_certificate_arn
   default_action {
     type = "fixed-response"
 
