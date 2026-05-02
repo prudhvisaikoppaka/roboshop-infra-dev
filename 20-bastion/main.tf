d2db59a4875e7d74b0b9c87e5baf5023e71911c0
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-02 12:55:15 +0530

    terraform infra

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,10 +4,15 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
 
+root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+}
+
   tags = merge(
     local.common_tags,
     {
       Name = "${var.project}-${var.environment}-bastion"
     }
   )
-} 
+}
