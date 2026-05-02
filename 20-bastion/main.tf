7590b245eb6975d2feb4045b62fb4649e220d4ea
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-02 13:01:44 +0530

    terraform infra

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "bastion" {
 # need more storage for terraform
 root_block_device {
     volume_size = 50
-    volume_type = "gp3"
+    volume_type = "gp3" # or "gp2", depending on your preference
 }
 
   tags = merge(
