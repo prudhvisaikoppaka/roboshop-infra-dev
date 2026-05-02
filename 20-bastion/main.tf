3dc8d9658efe9e3be361b8ea66c547f0cc293c4b
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-02 13:00:43 +0530

    terraform infra

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
 
+# need more storage for terraform
 root_block_device {
     volume_size = 50
     volume_type = "gp3"
