c8047cdc16735160230ea94e15edcd43b55a728a
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-05 23:11:27 +0530

    aws acm terraform infra

--- 60-acm/main.tf
+++ 60-acm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "prudhvisai" {
-  domain_name       = "dev.${var.zone_name}"
+  domain_name       = "${var.environment}.${var.zone_name}"
   validation_method = "DNS"
 
   tags = merge(
