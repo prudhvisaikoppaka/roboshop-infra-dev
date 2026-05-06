b57a0a675562e6cf97ba2021a7ac2af13e6503b5
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-06 11:00:47 +0530

    bastion terraform infra created

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -3,3 +3,7 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -L +20G /dev/RootVG/rootVol
+sudo lvextend -L +10G /dev/RootVG/varVol
