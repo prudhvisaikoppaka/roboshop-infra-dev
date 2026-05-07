c0fd8005012c7ea10e63bd5a79a583e47116ee8b
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-07 19:19:59 +0530

    aws cdn terraform infra cretaed

--- 91-cdn/variables.tf
+++ 91-cdn/variables.tf
@@ -6,9 +6,9 @@ variable "environment" {
   default = "dev"
 }
 
-# variable "zone_id" {
-#   default = "Z08496491D9BJEMR7DC94"
-# }
+variable "zone_id" {
+  default = "Z08496491D9BJEMR7DC94"
+}
 
 # variable "zone_name" {
 #   default = "prudhvisai.space"
