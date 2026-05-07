c946cbbd377df5c8c2dedb9f1f2d6d43ba97f527
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-07 19:21:17 +0530

    aws cdn terraform infra cretaed

--- 91-cdn/data.tf
+++ 91-cdn/data.tf
@@ -1,6 +1,6 @@
-# data "aws_cloudfront_cache_policy" "cacheEnable" {
-#   name = "Managed-CachingOptimized"
-# }
+data "aws_cloudfront_cache_policy" "cacheEnable" {
+  name = "Managed-CachingOptimized"
+}
 
 # data "aws_cloudfront_cache_policy" "cacheDisable" {
 #   name = "Managed-CachingDisabled"
